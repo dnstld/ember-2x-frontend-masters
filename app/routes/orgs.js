ae44add7be63591d3f9bbacc788b49ccd94a635e
Denis Toledo <contato@denistoledo.com.br>
2019-09-02 18:16:42 -0300

    routes, services and actions

--- app/routes/orgs.js
+++ app/routes/orgs.js
@@ -1,4 +1,37 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default Route.extend({
+  favorites: service(),
+
+  setupController() {
+    this._super(...arguments);
+  },
+  model() {
+    return [
+      {
+        id: 'emberjs'
+      },
+      {
+        id: 'ember-cli'
+      },
+      {
+        id: 'microsoft'
+      },
+      {
+        id: 'yahoo'
+      },
+      {
+        id: 'netflix'
+      },
+      {
+        id: 'facebook'
+      }
+    ];
+  },
+  actions: {
+    favoriteClicked(org) {
+      this.get('favorites').favoriteItem(org);
+    }
+  }
 });
---
+++ app/services/favorites.js
@@ -0,0 +1,10 @@
+import Service from '@ember/service';
+
+export default Service.extend({
+  items: [],
+
+  favoriteItem(item) {
+    this.get('items').addObject(item);
+    console.log(this.get('items').map(x => x.id).join(', '));
+  }
+});
